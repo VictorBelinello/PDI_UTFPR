14dbb23663224a148a1c563a147a662eeacd4a83
Victor Belinello <victorbelinello17@gmail.com>
2020-03-06 21:17:56 -0300

    Segmentation completed. T1 done.

--- T1/Python/main.py
+++ T1/Python/main.py
@@ -17,9 +17,9 @@ INPUT_IMAGE =  'arroz.bmp'
 # TODO: ajuste estes parâmetros!
 NEGATIVO = False
 THRESHOLD = 0.8
-ALTURA_MIN = 1
-LARGURA_MIN = 1
-N_PIXELS_MIN = 1
+ALTURA_MIN = 10
+LARGURA_MIN = 10
+N_PIXELS_MIN = 400
 
 #===============================================================================
 
@@ -34,6 +34,48 @@ Valor de retorno: versão binarizada da img_in.'''
     return np.where( img < threshold, 0.0, 1.0)
 
 #-------------------------------------------------------------------------------
+
+
+def inunda(label, rotulado, x0, y0, componente):
+    if(rotulado[y0][x0][0] == -1 ): # nao foi rotulado e nao eh pixel preto
+        # atribui label para o pixel
+        rotulado[y0][x0][0] = label
+
+        componente["label"] = label
+        componente["n_pixels"] = componente["n_pixels"] + 1 if "n_pixels" in componente else 1
+        componente["T"] = y0 if ( not ("T" in componente) or y0 < componente['T']) else componente['T']
+        componente["L"] = x0 if ( not ("L" in componente) or x0 < componente['L']) else componente['L']
+        componente["B"] = y0 if ( not ("B" in componente) or y0 > componente['B']) else componente['B']
+        componente["R"] = x0 if ( not ("R" in componente) or x0 > componente['R']) else componente['R']
+        
+
+        if x0 - 1 >= 0 and rotulado[y0][x0 - 1][0] == -1:
+            inunda(label, rotulado, x0 - 1, y0, componente)
+
+        if x0 + 1 < rotulado.shape[1] and rotulado[y0][x0 + 1][0] == -1:
+            inunda(label, rotulado, x0 + 1, y0, componente)
+
+        if y0 - 1 >= 0 and rotulado[y0 - 1][x0][0] == -1:
+            inunda(label, rotulado, x0, y0 - 1, componente)
+
+        if y0 + 1 < rotulado.shape[0] and rotulado[y0 + 1][x0][0] == -1: 
+            inunda(label, rotulado, x0, y0 + 1, componente)
+
+def componenteValido(componente, largura_min, altura_min, n_pixels_min):
+    if componente == {}:
+        return False
+
+    if componente["n_pixels"] < n_pixels_min:
+        return False
+    
+    if (componente["B"] - componente["T"]) < altura_min:
+        return False
+
+    if (componente["R"] - componente["L"]) < largura_min:
+        return False
+        
+
+    return True
 
 def rotula (img, largura_min, altura_min, n_pixels_min):
     '''Rotulagem usando flood fill. Marca os objetos da imagem com os valores
@@ -51,7 +93,21 @@ com os seguintes campos:
 'n_pixels': número de pixels do componente.
 'T', 'L', 'B', 'R': coordenadas do retângulo envolvente de um componente conexo,
 respectivamente: topo, esquerda, baixo e direita.'''
-    return []
+    altura, largura = img.shape[:2]
+    componentes = []
+    label = 0.1
+    img = np.where( img == 0, 0.0, -1.0)
+    componente_atual = { }
+
+    for y in range(altura):
+        for x in range(largura):
+            inunda(label, img, x,y, componente_atual)
+            if componenteValido(componente_atual, largura_min, altura_min, n_pixels_min):
+                componentes.append(componente_atual)
+                label += 0.1
+            componente_atual = { }
+
+    return componentes
     # TODO: escreva esta função.
     # Use a abordagem com flood fill recursivo.
 
@@ -72,7 +128,7 @@ def main ():
 
     # Mantém uma cópia colorida para desenhar a saída.
     img_out = cv2.cvtColor (img, cv2.COLOR_GRAY2BGR)
-
+    
     # Segmenta a imagem.
     if NEGATIVO:
         img = 1 - img
@@ -92,7 +148,7 @@ def main ():
 
     cv2.imshow ('02 - out', img_out)
     cv2.imwrite ('02 - out.png', img_out*255)
-    cv2.waitKey ()
+    cv2.waitKey (1000)
     cv2.destroyAllWindows ()
 
 
