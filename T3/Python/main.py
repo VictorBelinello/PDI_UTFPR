a7c23c5039bac575d728602b41d986667a8a7013
Victor Belinello <victorbelinello17@gmail.com>
2020-08-19 13:23:20 -0300

    T3 done.

--- T3/Python/main.py
+++ T3/Python/main.py
@@ -6,34 +6,76 @@ import cv2
 #INPUT_IMAGE = "Wind Waker GC.bmp"
 INPUT_IMAGE = "GT2.BMP"
 OUTPUT_NAME = INPUT_IMAGE[:-4]
-BRIGHT_THRESHOLD = 0.7
+BRIGHT_THRESHOLD = 0.5
 
-def bright_pass(img, option):
+# Qual metodo para gerar a mascara de fontes de luz
+# Caso coloque os 2 ira rodar com ambos
+BRIGHT_OPTIONS = ["HSL"] # NP_WHERE ou HSV
+
+# Qual metodo para borrar a mascara
+# Caso coloque os 2 ira rodar com ambos
+BLUR_OPTIONS = ["GAUSSIAN", "BOX_FILTER"] #GAUSSIAN ou BOX_FILTER
+
+NUMBER_BLURS = 5 #Quantas vezes a mascara sera borrada
+START_KSIZE = 5 #Tamanho inicial do kernel(deve ser impar), par nao funciona n função do opencv
+END_KSIZE = START_KSIZE + (NUMBER_BLURS*2) #ksize deve sempre ser impar
+
+def brightPass(img, option):
     if not isinstance(img, np.ndarray):
         raise Exception("img is not numpy ndarray")
-    if option == 0:
+    if option == "NP_WHERE":
+        # Mesmo resultado que cv2.threshold(img, BRIGHT_THRESHOLD, 1, cv2.THRESH_TOZERO)
         return np.where(img > BRIGHT_THRESHOLD, img, 0)
-    else:
+    elif option == "HSL":
         imgHLS = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-        imgL = imgHLS[:,:,1]
-        mask = cv2.inRange(imgL, BRIGHT_THRESHOLD, 1)
-        return cv2.bitwise_and(img, img, mask=mask)
+        mask = cv2.inRange(imgHLS[:,:,1], BRIGHT_THRESHOLD, 1)
+        return cv2.bitwise_or(img, img, mask=mask)
+    else:
+        raise Exception(f"bright option '{option}' is not valid.")
 
 def blur(img, option):
     if not isinstance(img, np.ndarray):
         raise Exception("img is not numpy ndarray")
     blurImg = np.zeros(img.shape)
-    MAX_LOOPS = 9
-    if option == 0:
-        for i in range(1,MAX_LOOPS,2):
-            ksize = (5 * i, 5 * i)
-            blurImg += cv2.GaussianBlur(img, ksize, 3)
+    
+    if option == "GAUSSIAN":
+        for i in range(START_KSIZE,END_KSIZE,2): 
+            blurImg += cv2.GaussianBlur(img, (i, i), 3)
+            print("Vezes")
+        return blurImg
+    elif option == "BOX_FILTER":
+        for i in range(START_KSIZE,END_KSIZE,2):
+            blurImg += cv2.boxFilter(img, -1, (i, i))
         return np.where(blurImg > 1, 1, blurImg)
     else:
-        for i in range(1,MAX_LOOPS,2):
-            kernel = np.ones((5*i,5*i), np.float32)/(25*i*i)
-            blurImg += cv2.filter2D(img, -1, kernel)
-        return np.where(blurImg > 1, 1, blurImg)
+        raise Exception(f"blur option '{option}' is not valid")
+
+def bloom(img):
+    cv2.imshow(OUTPUT_NAME, img)
+
+    brightImages = []    
+    for option in BRIGHT_OPTIONS:
+        brightImages.append( brightPass(img, option) )
+    for idx, bright in enumerate(brightImages):
+        cv2.imshow(f"{OUTPUT_NAME} brightPass {idx}" , bright)
+   
+
+    blurredImages = []
+    for option in BLUR_OPTIONS:
+        for bright in brightImages:
+            blurredImages.append( blur(bright, option))
+    for idx, blurred in enumerate(blurredImages):
+        cv2.imshow(f"{OUTPUT_NAME} blurred {idx}" , blurred)
+    
+    outputImages = []
+    for blurredImg in blurredImages:
+        out = img + 0.7*blurredImg
+        outputImages.append( out )
+
+    for idx, output in enumerate(outputImages):
+        cv2.imshow(f"{OUTPUT_NAME} bloom {idx}" , output)
+
+    
 
 def main():
     img = cv2.imread(INPUT_IMAGE, cv2.IMREAD_COLOR)
@@ -42,46 +84,14 @@ def main():
         print('Erro abrindo a imagem.\n')
         sys.exit()
     
-    # É uma boa prática manter o shape com 3 valores, independente da imagem ser
-    # colorida ou não. Também já convertemos para float32.
+    # Converte para float32.
     img = img.astype(np.float32) / 255
     
-    cv2.imshow(OUTPUT_NAME, img)
-    cv2.waitKey(2000)
-    
-    bright = bright_pass(img, 1)
-    cv2.imshow(OUTPUT_NAME, bright)
-    cv2.waitKey(2000)
+    bloom(img)
+    # O numero de resultados eh dado por: len(BRIGHT_OPTIONS) * len(BLUR_OPTIONS)
+    # Os indices que aparecem no titulo das imagens correspondem a posicao na respectiva lista de opcoes
 
-    option = 1
-    if option == 1:
-        blured = blur(bright, 1)
-        cv2.imshow(OUTPUT_NAME, blured)
-        cv2.waitKey(2000)
-        out = 0.7*blured + img
-        
-        cv2.imshow(OUTPUT_NAME, out)
-        cv2.waitKey(2000)
-
-        out = 0.3*blured + img
-        
-        cv2.imshow(OUTPUT_NAME, out)
-        cv2.waitKey(2000)
-    else:
-        blured = blur(bright, 0)
-        cv2.imshow(OUTPUT_NAME, blured)
-        cv2.waitKey(2000)
-        out = blured + img
-        cv2.imshow(OUTPUT_NAME, out)
-        cv2.waitKey(2000)
-    # out, out2 = bright_pass(img)
-    # cv2.imshow(OUTPUT_NAME,out )
-
-    # cv2.waitKey(2000)
-
-    # cv2.imshow(OUTPUT_NAME,out2 )
-    # cv2.waitKey(2000)
-
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
